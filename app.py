717e203ffbfc74e06476b486d3e6274ff97dc2ac
Pol Alvarez <pol.avms@gmail.com>
2024-04-19 18:29:24 +0200

    feat: alpha graph version in prod

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, jsonify, request
 import os
 from graphql_api import handle_graphql_request
 from flask_cors import CORS
-
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
@@ -13,11 +14,8 @@ def health():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/graph')
-def graph():
-    return render_template('graph.html')
+    api_url = os.getenv("API_URL", "http://127.0.0.1:5001/graphql")
+    return render_template('index.html', api_url=api_url)
 
 # Add the GraphQL endpoint
 @app.route('/graphql', methods=['POST'])
